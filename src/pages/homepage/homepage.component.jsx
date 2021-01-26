74a10d140c15985803d7e7438998557338597a8a
youneslk <youneslamkaddemwork@gmail.com>
2021-01-26 15:10:56 +0100

    second contribution

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './homepage.style.scss'
+import Directory from '../../components/diectory/directory.component'
+const Homepage = () => (
+    <div className="homepage">
+        <Directory />
+    </div>
+)
+
+export default Homepage;
